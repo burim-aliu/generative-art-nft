9bc7b5521955426d44f6f3bf49bb5ba5bca55151
burim-aliu <burimaliu2023@gmail.com>
2022-02-28 19:32:17 +0100

    Init commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# Generative NFT Art
+
+## Introduction
+
+The `generative-art-nft` repository is a library for creating generative art. It was developed for the purpose of creating NFT avatar & collectible projects.
+
+## Features
+
+### Generate over a million distinct images with less than 60 traits
+The library allows you to generate images every distinct possible combination of your traits. For context, if you had trait art for a project like [Bored Apes](https://boredapeyachtclub.com/#/home), the library could generate upwards of 1.2 billion distinct apes.
+
+### Add rarity weights
+The library also allows you to configure the image generation process in such a way that you have complete control over how rare each and every trait is.
+
+### Generate compliant JSON metadata for your NFTs
+There is now an added functionality to generate JSON metadata for your NFTs that are in compliance with OpenSea metadata requirements (and by extension, the general NFT metadata standard).
+
+## Installation
+
+**Clone this repository**
+
+```git clone https://github.com/burim-aliu/generative-art-nft.git```
+
+**Install required packages**
+
+```pip install Pillow pandas progressbar2```
+```yarn add ipfs-car```
+```yarn add carbites-cli```
+
+
+Upload your input assets in the `assets` folder, fill up the `config.py` file, and then run `python nft.py`.
+
+In order to generate JSON metadata, define BASE_NAME, BASE_IMAGE_URL, and BASE_JSON in `metadata.py` and then run `python metadata.py`.
+
+In order to generate .car file for NFT.storage, run `yarn ipfs-car --pack <input directory> --output <output>`
+
+In order to split CARs, run `carbites split --size 100MB --strategy treewalk ***.car`
+
+In order to join CARs, run `carbites join <example-*.car> --output <example-joined.car>`
+
+
+
+
+
---
+++ config.py
@@ -0,0 +1,66 @@
+# This file MUST be configured in order for the code to run properly
+
+# Make sure you put all your input images into an 'assets' folder. 
+# Each layer (or category) of images must be put in a folder of its own.
+
+# CONFIG is an array of objects where each object represents a layer
+# THESE LAYERS MUST BE ORDERED.
+
+# Each layer needs to specify the following
+# 1. id: A number representing a particular layer
+# 2. name: The name of the layer. Does not necessarily have to be the same as the directory name containing the layer images.
+# 3. directory: The folder inside assets that contain traits for the particular layer
+# 4. required: If the particular layer is required (True) or optional (False). The first layer must always be set to true.
+# 5. rarity_weights: Denotes the rarity distribution of traits. It can take on three types of values.
+#       - None: This makes all the traits defined in the layer equally rare (or common)
+#       - "random": Assigns rarity weights at random. 
+#       - array: An array of numbers where each number represents a weight. 
+#                If required is True, this array must be equal to the number of images in the layer directory. The first number is  the weight of the first image (in alphabetical order) and so on...
+#                If required is False, this array must be equal to one plus the number of images in the layer directory. The first number is the weight of having no image at all for this layer. The second number is the weight of the first image and so on...
+
+# Be sure to check out the tutorial in the README for more details.                
+
+CONFIG = [
+    {
+        'id': 1,
+        'name': 'background',
+        'directory': 'Layer 1 - background',
+        'required': True,
+        'rarity_weights': None,
+    },
+    {
+        'id': 2,
+        'name': 'eyes',
+        'directory': 'Layer 2 - Eyes',
+        'required': True,
+        'rarity_weights': None,
+    },
+    {
+        'id': 3,
+        'name': 'tshirt',
+        'directory': 'Layer 3 - T-Shirt',
+        'required': True,
+        'rarity_weights': None,
+    },
+    {
+        'id': 4,
+        'name': 'chain',
+        'directory': 'Layer 4 - Chain',
+        'required': True,
+        'rarity_weights': [99875,125]
+    },
+    {
+        'id': 5,
+        'name': 'wear',
+        'directory': 'Layer 5 - Wear',
+        'required': True,
+        'rarity_weights': None,
+    },
+    {
+        'id': 6,
+        'name': 'baboons',
+        'directory': 'Layer 6 - Baboons',
+        'required': True,
+        'rarity_weights': None,
+    },
+]
